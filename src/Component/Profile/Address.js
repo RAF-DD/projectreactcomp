74958c32021548a19317cc4ccea6dd7a0377ae52
RAF-DD <85300478+RAF-DD@users.noreply.github.com>
2021-08-12 17:22:37 +0100

    Add files via upload

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Address = () => {
+    return (
+        <div>
+           <h2 className='addres' >Sousse</h2> 
+        </div>
+    )
+}
+
+export default Address
---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import img1 from './img.jpg';
+const ProfilePhoto = () => {
+    return (
+        <div>
+            <img src={img1} />
+        </div>
+    )
+}
+
+export default ProfilePhoto;
+
